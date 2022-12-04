a9b69afff35a359764638f9afba48796f5165557
Dhea Gitawijaya <dheagitawijaya@mhs.pelitabangsa.ac.id>
2022-12-04 13:11:23 +0700

    file php

---
+++ lab9_php_modular/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Modularisasi</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet"
+media="screen" />
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Modularisasi Menggunakan Require</h1>
+        </header>
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="about.php">Tentang</a>
+            <a href="kontak.php">Kontak</a>
+        </nav>
+<?php //require('header.php'); ?>
+    <div class="content">
+        <h2>Ini Halaman Home</h2>
+        <p>Ini adalah bagian content dari halaman.</p>
+    </div>
+<?php //require('footer.php'); ?>
+        <footer>
+            <p>&copy; 2022, Informatika, Universitas Pelita Bangsa</p>
+        </footer>
+    </div>
+</body>
+</html>
